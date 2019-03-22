4c488f5a663ca2596b6a0741b5695f4d0bafb6c7
Masila <abelmasila@gmail.com>
2019-03-22 18:38:30 +0300

    added navbar and routes

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
 
+
+# about page
+def about(request):
+    return render(request, 'about.html', {"name": 'Masila'})
