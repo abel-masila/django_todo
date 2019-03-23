7c793ba7d831a3bb9761c6a993aa5e4de9591cc2
Masila <abelmasila@gmail.com>
2019-03-23 15:56:31 +0300

    added alert from view

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
-    all_items = List.objects.all
-    context = {'all_items': all_items}
-    return render(request, 'home.html', context)
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            context = {'all_items': all_items}
+            messages.success(request, "Item has been added to List! ")
+            return render(request, 'home.html', context)
+    else:
+        all_items = List.objects.all
+        context = {'all_items': all_items}
+        return render(request, 'home.html', context)
 
 
 # about page
