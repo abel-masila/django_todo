7da4a4fee86bac31f027b7d4f00606a5b3631d01
Masila <abelmasila@gmail.com>
2019-03-23 14:02:18 +0300

    connected database to our home page

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import List
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    all_items = List.objects.all
+    context = {'all_items': all_items}
+    return render(request, 'home.html', context)
 
 
 # about page
